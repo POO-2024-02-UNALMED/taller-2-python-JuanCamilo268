1573b6c47dad9c83bbe942751e7256f3fcefc1bd
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-10 18:35:00 -0500

    Update Asiento.py

---
+++ Asiento.py
@@ -0,0 +1,25 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(nColor):
+        nColor = nColor.lower()
+        if nColor == "rojo":
+            this.color = nColor
+
+        elif nColor == "verde":
+            this.color == nColor
+        
+        elif nColor == "amarillo":
+            this.color == nColor
+
+        elif nColor == "blanco":
+            this.color == nColor
+
+        elif nColor == "negro":
+            this.color == nColor
+       
+        
+        
